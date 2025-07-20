a981ad78ffaf0e8fb6629f039ee3184c48e43448
Jose Murillo <jooseemurillo@gmail.com>
2025-07-20 11:54:17 -0300

    feat: reset blur-header and fiz size

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,18 @@ const hamburger = document.getElementById('menu-hamburger-button');
 const app = document.querySelector('.app');
 
 hamburger.onclick = () => {
+  const headerElement = document.querySelector('.header');
   const drawer = document.getElementById("drawer-mobile");
-  
+
+
   drawer.classList.toggle("change");
+  headerElement.classList.toggle('no-blur-header');
   document.body.classList.toggle('no-scroll');
+  
 };
 
 function setAppHeightMode () {
   if (isMobileUserAgent()) {
-    console.log({ app })
     app.style.height = '100vh';
   }
 }
